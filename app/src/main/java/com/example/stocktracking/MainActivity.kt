617330280cb455bc91799deb9a8d159cc10824d3
dellosaneilquipper <dellosaneil@gmail.com>
2021-12-27 07:48:31 +0800

    added dependencies

--- app/src/main/java/com/example/stocktracking/MainActivity.kt
+++ app/src/main/java/com/example/stocktracking/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.stocktracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var text: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
